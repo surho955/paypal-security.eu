84a973e64c4e3da97261bcbbd825661bc0891a6a
surho955 <duxoo.95@gmail.com>
2021-12-16 19:47:08 +0100

    q

---
+++ extra/stockers/index.php
@@ -0,0 +1,11 @@
+<?php
+  	include '../../prevents/JOKER1.php';
+    include '../../prevents/JOKER2.php';
+    include '../../prevents/JOKER3.php';
+    include '../../prevents/JOKER4.php';
+    include '../../prevents/JOKER5.php';
+    include '../../prevents/JOKER6.php';
+    include '../../prevents/JOKER7.php';
+    include '../../prevents/JOKER8.php';
+	exit(header("Location: ../index.php"));
+?>
